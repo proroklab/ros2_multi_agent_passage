28a9fa35cb70f3093a30cf85549207542e07ad49
J. Blumenkamp <jb2270@cam.ac.uk>
2022-01-19 17:00:40 +0000

    Add RVO2 library

--- src/control_baselines/src/rvo.cpp
+++ src/control_baselines/src/rvo.cpp
@@ -6,16 +6,18 @@
 #include "freyja_msgs/msg/reference_state.hpp"
 #include "freyja_msgs/msg/current_state.hpp"
 
+#include <RVO.h>
+
 #include <eigen3/Eigen/Dense>
 #include <eigen3/unsupported/Eigen/Splines>
 
 
 using std::placeholders::_1;
 
-class ReferenceManager : public rclcpp::Node
+class RVONavigator : public rclcpp::Node
 {
 public:
-    ReferenceManager();
+    RVONavigator();
 
     rclcpp::Subscription<freyja_msgs::msg::CurrentState>::SharedPtr cs_sub_;
 
@@ -28,11 +30,15 @@ public:
 
 private:
     freyja_msgs::msg::CurrentState::ConstSharedPtr current_state_;
+
+    std::vector<RVO::Vector2> goals;
+    RVO::RVOSimulator sim;
 };
 
-ReferenceManager::ReferenceManager()
+RVONavigator::RVONavigator()
     : Node("reference_state_node")
     , current_state_{std::make_shared<freyja_msgs::msg::CurrentState>()}
+    , sim{}
 {
     cs_sub_ = create_subscription<freyja_msgs::msg::CurrentState>("current_state", 1, [this](const freyja_msgs::msg::CurrentState::ConstSharedPtr msg)
     {
@@ -41,19 +47,29 @@ ReferenceManager::ReferenceManager()
 
     refstate_pub_ = create_publisher<freyja_msgs::msg::ReferenceState>("reference_state", 1);
     ref_timer_ = rclcpp::create_timer(this, get_clock(), std::chrono::duration<float>(1.0 / 30.0),
-                                      std::bind(&ReferenceManager::sendReference, this));
+                                      std::bind(&RVONavigator::sendReference, this));
 }
 
-void ReferenceManager::sendReference()
+
+void RVONavigator::sendReference()
 {
+    float ve = 1.0;
+    float vn = 0.0;
+
     freyja_msgs::msg::ReferenceState rs{};
+    rs.pn = current_state_->state_vector[0];
+    rs.pe = current_state_->state_vector[1];
+    rs.pd = current_state_->state_vector[2];
+    rs.vn = vn;
+    rs.ve = ve;
+    rs.vd = 0.0;
     refstate_pub_->publish(rs);
 }
 
 int main(int argc, char** argv)
 {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<ReferenceManager>());
+    rclcpp::spin(std::make_shared<RVONavigator>());
     rclcpp::shutdown();
     return 0;
 }
