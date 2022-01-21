daeb3507327d2ad234b50d6eaf5b042b7b9fbfeb
J. Blumenkamp <jb2270@cam.ac.uk>
2022-01-21 15:24:10 +0000

    Basic multi-agent subscriber/publisher for freyja

--- src/control_baselines/src/rvo.cpp
+++ src/control_baselines/src/rvo.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <map>
 
 #include "rclcpp/rclcpp.hpp"
 #include "freyja_msgs/msg/reference_state.hpp"
@@ -14,56 +15,79 @@
 
 using std::placeholders::_1;
 
+struct Agent
+{
+    Agent(rclcpp::Node* node, const std::string& ns)
+        : ns_{ns}
+        , node_{node}
+        , current_state_{}
+    {
+        cs_sub_ = node->create_subscription<freyja_msgs::msg::CurrentState>(ns + "/current_state", 1, [this](const freyja_msgs::msg::CurrentState::ConstSharedPtr msg)
+        {
+            current_state_ = *msg;
+        });
+
+        refstate_pub_ = node->create_publisher<freyja_msgs::msg::ReferenceState>(ns + "/reference_state", 1);
+    }
+
+    const std::string ns_;
+    rclcpp::Node* node_;
+    rclcpp::Subscription<freyja_msgs::msg::CurrentState>::SharedPtr cs_sub_;
+
+    rclcpp::Publisher<freyja_msgs::msg::ReferenceState>::SharedPtr refstate_pub_;
+    freyja_msgs::msg::CurrentState current_state_;
+};
+
 class RVONavigator : public rclcpp::Node
 {
 public:
     RVONavigator();
 
-    rclcpp::Subscription<freyja_msgs::msg::CurrentState>::SharedPtr cs_sub_;
-
-    void csCallback(const freyja_msgs::msg::CurrentState::ConstSharedPtr);
-
-    rclcpp::Publisher<freyja_msgs::msg::ReferenceState>::SharedPtr refstate_pub_;
     rclcpp::TimerBase::SharedPtr ref_timer_;
 
     void sendReference();
 
 private:
-    freyja_msgs::msg::CurrentState::ConstSharedPtr current_state_;
 
+    std::vector<std::unique_ptr<Agent>> agents_;
     std::vector<RVO::Vector2> goals;
     RVO::RVOSimulator sim;
 };
 
 RVONavigator::RVONavigator()
     : Node("reference_state_node")
-    , current_state_{std::make_shared<freyja_msgs::msg::CurrentState>()}
     , sim{}
 {
-    cs_sub_ = create_subscription<freyja_msgs::msg::CurrentState>("current_state", 1, [this](const freyja_msgs::msg::CurrentState::ConstSharedPtr msg)
-    {
-        current_state_ = msg;
-    });
-
-    refstate_pub_ = create_publisher<freyja_msgs::msg::ReferenceState>("reference_state", 1);
     ref_timer_ = rclcpp::create_timer(this, get_clock(), std::chrono::duration<float>(1.0 / 30.0),
                                       std::bind(&RVONavigator::sendReference, this));
+
+    for (const auto& i :
+         {
+             0, 1
+         })
+    {
+        agents_.emplace_back(std::make_unique<Agent>(this, "robomaster_" + std::to_string(i)));
+    }
 }
 
 
 void RVONavigator::sendReference()
 {
-    float ve = 1.0;
+    float ve = -0.2;
     float vn = 0.0;
 
-    freyja_msgs::msg::ReferenceState rs{};
-    rs.pn = current_state_->state_vector[0];
-    rs.pe = current_state_->state_vector[1];
-    rs.pd = current_state_->state_vector[2];
-    rs.vn = vn;
-    rs.ve = ve;
-    rs.vd = 0.0;
-    refstate_pub_->publish(rs);
+    for (auto& agent : agents_)
+    {
+        freyja_msgs::msg::ReferenceState rs{};
+        rs.pn = agent->current_state_.state_vector[0];
+        rs.pe = agent->current_state_.state_vector[1];
+        rs.pd = agent->current_state_.state_vector[2];
+        rs.vn = vn;
+        rs.ve = ve;
+        rs.vd = 0.0;
+        agent->refstate_pub_->publish(rs);
+        std::cout << agent->ns_ << " " << agent->current_state_.state_vector[0] << " " << agent->current_state_.state_vector[1] << "\n";
+    }
 }
 
 int main(int argc, char** argv)
