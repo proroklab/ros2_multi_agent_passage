ae7bc1855f901e9481fab86f80845d0a4d059214
J. Blumenkamp <jb2270@cam.ac.uk>
2022-02-09 17:33:03 +0000

    Implement step function for RVO

--- src/evaluation_infrastructure/evaluation_infrastructure/agent_centralized_robomaster_rvo_passage.py
+++ src/evaluation_infrastructure/evaluation_infrastructure/agent_centralized_robomaster_rvo_passage.py
@@ -2,11 +2,56 @@ import rclpy
 from typing import Dict, List
 from evaluation_infrastructure.agent_util import get_uuids_fast
 from evaluation_infrastructure.agent_start_goal import AgentStartGoal
+from rclpy.action import ActionClient
+from evaluation_msgs.action import PoseControl
+from functools import partial
 
 
 class AgentCentralizedRobomasterRVOPassage(AgentStartGoal):
+    _action_clients_rvo_step: Dict[str, ActionClient] = {}
+    _step_dones: Dict[str, bool] = {}
+
     def __init__(self):
         super().__init__()
+
+    def rvo_step_goal_response_callback(self, uuid, future):
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            self.get_logger().info("Rejected navigation request")
+            self._step_dones[uuid] = True
+            return
+
+        def get_result_callback(_):
+            self._step_dones[uuid] = True
+
+        future = goal_handle.get_result_async()
+        future.add_done_callback(get_result_callback)
+
+    def on_global_mode_transition(self, old_mode, new_mode):
+        super().on_global_mode_transition(old_mode, new_mode)
+
+        if new_mode == self.MODE.RESETTING:
+            self._step_dones = {}
+
+    def step(
+        self, controllable_agents: List[str], state_dict: Dict[str, Dict]
+    ) -> Dict[str, bool]:
+        for agent in controllable_agents:
+            if agent not in self._action_clients_rvo_step:
+                self._action_clients_rvo_step[agent] = ActionClient(
+                    self, PoseControl, f"/{agent}/pose_control"
+                )
+
+            if agent not in self._step_dones:
+                g = PoseControl.Goal()
+                g.goal_pose = self.goal_poses[agent]
+                future = self._action_clients_rvo_step[agent].send_goal_async(g)
+                future.add_done_callback(
+                    partial(self.rvo_step_goal_response_callback, agent)
+                )
+                self._step_dones[agent] = False
+
+        return self._step_dones
 
     def get_controllable_agents(self) -> List[str]:
         """Override me. Return uuids of the agents we control"""
